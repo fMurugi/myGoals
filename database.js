5731623c99cd658ae80118de83c71aaf4a65d809
fMurugi <fionamurugi99@gmail.com>
2022-10-04 13:37:41 +0300

    Added .env file

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 var mysql = require('mysql');
 var conn = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'rootpassword',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: 'mygoals' 
 }); 
 conn.connect(function(err) {
